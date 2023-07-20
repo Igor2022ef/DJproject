a5a9aae2090f8cba4872dc752e17689c1d5b0517
Igor2022ef <2023ief@gmail.com>
2023-07-20 18:17:45 +0300

    working with templates, including base

--- honeststat/diffinform/models.py
+++ honeststat/diffinform/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+class Articles(models.Model):
+    title = models.CharField(max_length=255, verbose_name='Заголовок')
+    # slug = models.SlugField(max_length=255, unique=True, db_index=True, verbose_name="URL")
+    content = models.TextField(blank=True, verbose_name='Текст статьи')
+    photo = models.ImageField(blank=True, upload_to="photos/%Y/%m/%d/", verbose_name='Рисунок графика')
+    time_create = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
+    time_update = models.DateTimeField(auto_now=True, verbose_name='Время изменения')
+    # cat = models.ForeignKey('Category', on_delete=models.PROTECT, verbose_name='Категория')
+    is_published = models.BooleanField(default=True, verbose_name='Публикация')
+
+    def __str__(self):
+        return self.title
+
+# class Category(models.Model):
+#     name = models.CharField(max_length=100, db_index=True, verbose_name='Название категории')
+#     slug = models.SlugField(max_length=255, unique=True, db_index=True, verbose_name="URL")
+#
+#
+# class Graf(models.Model):
+#     title = models.CharField(max_length=255, verbose_name='Заголовок')
+#     slug = models.SlugField(max_length=255, unique=True, db_index=True, verbose_name="URL")
+#     year = models.DateField(max_length=4)
+#     index = models.DecimalField(blank=True,max_digits=32, decimal_places=2)
+#     index_name = models.CharField(max_length=255)
+#     time_create = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
+#     time_update = models.DateTimeField(auto_now=True, verbose_name='Время изменения')
+#     cat = models.ForeignKey('Category', on_delete=models.PROTECT, verbose_name='Категория')
+#     articles = models.ForeignKey('Articles', on_delete=models.PROTECT, verbose_name='Статья')
+#     is_published = models.BooleanField(default=True, verbose_name='Публикация')
+
--- honeststat/diffinform/views.py
+++ honeststat/diffinform/views.py
@@ -1,17 +1,25 @@
 from django.http import HttpResponse, HttpResponseNotFound
 from django.shortcuts import render, redirect
+from .models import *
 
+menu = ["О сайте", "Добавить статью", "Обратная связь", "Войти"]
 
 def index(request):
-    return HttpResponse("Главная страница приложения.")
+    posts = Articles.objects.all()
+    return render(request, 'diffinform/index.html', {'posts': posts,'menu': menu,'title': 'Главная страница'})
 
-def categories(request, cat):
-    return HttpResponse(f"<h1>Статьи по категориям</h1><p>{cat}</p>")
+def about(request):
+    return render(request, 'diffinform/about.html', {'title': 'О сайте'})
+
+# def categories(request, cat):
+#     if (request.GET):
+#         print(request.GET)
+#     return HttpResponse(f"<h1>Статьи по категориям</h1>{cat}</p>")
+
+# def archive(request, year):
+#     if (int(year) > 2020):
+#         return redirect('/')
+#     return HttpResponse(f"<h1>Архив по годам</h1>{year}</p>")
 
 def pageNotFound(request, exception):
-    return HttpResponseNotFound('<h1>Страница не найдена</h1>')
-
-def archive(request, year):
-    if (int(year) > 2020):
-        return redirect('home', permanent=True)
-    return HttpResponse(f"<h1>Архив по годам</h1>{year}</p>")
+    return HttpResponseNotFound("<h1>Страница не найдена</h1>")
