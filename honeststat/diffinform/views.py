cc717b675e94bf8e8397a7269713a1d2c44d9a33
Igor2022ef <2023ief@gmail.com>
2023-07-21 17:46:14 +0300

    working with urls in templates

--- honeststat/diffinform/views.py
+++ honeststat/diffinform/views.py
@@ -2,14 +2,38 @@ from django.http import HttpResponse, HttpResponseNotFound
 from django.shortcuts import render, redirect
 from .models import *
 
-menu = ["О сайте", "Добавить статью", "Обратная связь", "Войти"]
+menu = [{'title': "О сайте", 'url_name': 'about'},
+        {'title': "Добавить статью", 'url_name': 'add_page'},
+        {'title': "Обратная связь", 'url_name': 'contact'},
+        {'title': "Войти", 'url_name': 'login'}
+]
 
 def index(request):
     posts = Articles.objects.all()
-    return render(request, 'diffinform/index.html', {'posts': posts,'menu': menu,'title': 'Главная страница'})
+    context = {
+        'posts': posts,
+        'menu': menu,
+        'title': 'Главная страница'
+    }
+    return render(request, 'diffinform/index.html', context=context)
 
 def about(request):
-    return render(request, 'diffinform/about.html', {'title': 'О сайте'})
+    return render(request, 'diffinform/about.html', {'menu': menu, 'title': 'О сайте'})
+
+
+def addpage(request):
+    return HttpResponse("Добавление статьи")
+
+
+def contact(request):
+    return HttpResponse("Обратная связь")
+
+
+def login(request):
+    return HttpResponse("Авторизация")
+
+def show_post(request, post_id):
+    return HttpResponse(f"Отображение статьи с id = {post_id}")
 
 # def categories(request, cat):
 #     if (request.GET):
