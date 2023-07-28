1f37fb5b9a22a2e7147740866848f663dfe946c4
Igor2022ef <2023ief@gmail.com>
2023-07-28 17:53:43 +0300

    working with inclusion tags

--- honeststat/diffinform/templatetags/diffinform_tags.py
+++ honeststat/diffinform/templatetags/diffinform_tags.py
@@ -6,3 +6,8 @@ register = template.Library()
 @register.simple_tag(name='getcats')
 def get_categories():
     return Category.objects.all()
+
+@register.inclusion_tag('diffinform/list_categories.html')
+def show_categories(cat_selected):
+    cats = Category.objects.all()
+    return {"cats": cats, 'cat_selected': cat_selected}
