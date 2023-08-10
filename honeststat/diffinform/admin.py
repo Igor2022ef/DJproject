0c84186ab004d22b2ee07a7583d72ba90b6ebb6a
Igor2022ef <2023ief@gmail.com>
2023-08-10 13:22:10 +0300

    working, category selection by slug

--- honeststat/diffinform/admin.py
+++ honeststat/diffinform/admin.py
@@ -1,12 +1,23 @@
 from django.contrib import admin
-
 from .models import *
 
-class ArticlesAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'time_create', 'photo', 'is_published')
-    list_display_links = ('id', 'title')
-    search_fields = ('title', 'content')
-    prepopulated_fields = {"slug": ("title",)}
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
+
+
+class ArticlesResource(resources.ModelResource):
+    class Meta:
+        model = Articles
+        exclude = ('title', 'slug', 'content', 'photo', 'time_create', 'cat', 'time_update', 'is_published')
+# вывод данных на странице
+class ArticlesAdmin(ImportExportModelAdmin):
+    resource_classes = [ArticlesResource]
+    list_display = ('title', 'name', 'is_published')
+    search_fields = ('title', 'name')
+    # prepopulated_fields = {"slug": ("title",)}
+
+admin.site.register(Articles, ArticlesAdmin)
+
 
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('id', 'name')
@@ -14,5 +25,7 @@ class CategoryAdmin(admin.ModelAdmin):
     search_fields = ('name',)
     prepopulated_fields = {"slug": ("name",)}
 
-admin.site.register(Articles, ArticlesAdmin)
+
 admin.site.register(Category, CategoryAdmin)
+
+
