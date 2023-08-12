17722386fd675a84f35f581e3867298472415886
Igor2022ef <2023ief@gmail.com>
2023-08-12 11:21:33 +0300

    changed model

--- honeststat/diffinform/models.py
+++ honeststat/diffinform/models.py
@@ -4,12 +4,17 @@ from django.urls import reverse
 
 class Articles(models.Model):
     title = models.CharField(max_length=255, verbose_name='Заголовок')
-    slug = models.SlugField(max_length=255, unique=True, db_index=True, verbose_name="URL")
+    slug = models.SlugField(max_length=255, db_index=True, verbose_name="URL") #unique=True,
     content = models.TextField(blank=True, verbose_name='Текст статьи')
+    name = models.CharField(max_length=150, verbose_name='Название товара', blank=True)
+    cost = models.CharField(max_length=150, blank=True)
+    weight = models.CharField(max_length=150, blank=True)
+    date = models.CharField(max_length=150, blank=True)
+    shop = models.TextField(null=True,blank=True)
     photo = models.ImageField(blank=True, upload_to="photos/%Y/%m/%d/", verbose_name='Рисунок графика')
     time_create = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
     time_update = models.DateTimeField(auto_now=True, verbose_name='Время изменения')
-    cat = models.ForeignKey('Category', on_delete=models.PROTECT, verbose_name='Категория')
+    cat = models.ForeignKey('Category', on_delete=models.PROTECT, verbose_name='Категория', null=True)
     is_published = models.BooleanField(default=True, verbose_name='Публикация')
 
     def __str__(self):
@@ -17,6 +22,9 @@ class Articles(models.Model):
 
     def get_absolute_url(self):
         return reverse('post', kwargs={'post_slug': self.slug})
+
+class Meta:
+    ordering = ['title']
 
 class Category(models.Model):
     name = models.CharField(max_length=100, db_index=True, verbose_name='Название категории')
@@ -28,17 +36,5 @@ class Category(models.Model):
     def get_absolute_url(self):
         # return reverse('category', kwargs={'cat_id': self.pk})
         return reverse('category', kwargs={'cat_slug': self.slug})
-#
-#
-# class Graf(models.Model):
-#     title = models.CharField(max_length=255, verbose_name='Заголовок')
-#     slug = models.SlugField(max_length=255, unique=True, db_index=True, verbose_name="URL")
-#     year = models.DateField(max_length=4)
-#     index = models.DecimalField(blank=True,max_digits=32, decimal_places=2)
-#     index_name = models.CharField(max_length=255)
-#     time_create = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
-#     time_update = models.DateTimeField(auto_now=True, verbose_name='Время изменения')
-#     cat = models.ForeignKey('Category', on_delete=models.PROTECT, verbose_name='Категория')
-#     articles = models.ForeignKey('Articles', on_delete=models.PROTECT, verbose_name='Статья')
-#     is_published = models.BooleanField(default=True, verbose_name='Публикация')
+
 
